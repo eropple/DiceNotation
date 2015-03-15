4bcfe75ca5760eb609eaffb0e8b980a2f50564e9
Ed Ropple <ed@edropple.com>
2015-03-15 15:42:47 -0400

    Added DiceNotation.MathNet to support MathNet.Numerics.Random.RandomSource.

---
+++ DiceNotation.MathNet/Rollers/RandomSourceRoller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DiceNotation.Rollers;
+using JetBrains.Annotations;
+using MathNet.Numerics.Random;
+
+namespace DiceNotation.MathNet.Rollers
+{
+    public class RandomSourceRoller : IDieRoller
+    {
+        private readonly RandomSource _source;
+
+        public RandomSourceRoller([NotNull] RandomSource source)
+        {
+            _source = source;
+        }
+
+        public Int32 RollDie(int sides)
+        {
+            return _source.Next(0, sides) + 1;
+        }
+    }
+}
